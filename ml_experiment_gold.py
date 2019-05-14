3952e49d8e757596324ab02d63684aeea23a7598
Ellis Brown <ellis.brown@blackrock.com>
2019-05-13 21:52:23 -0400

    .

--- ml_experiment_gold.py
+++ ml_experiment_gold.py
@@ -57,7 +57,7 @@ epochs = args.epochs
 epoch_step_size = 200
 batch_size = 128
 
-test_iters = 20 # low to speed up training
+test_iters = 20  # low to speed up training
 n_std = 2
 
 tau = .10  # 10
